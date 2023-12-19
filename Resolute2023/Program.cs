f0648e8745fbbb58322dea3590768cc96bad955c
Nikola <Nikola@opspro.co.za>
2023-12-19 17:18:26 +0100

    add d

--- Resolute2023/Program.cs
+++ Resolute2023/Program.cs
@@ -6,8 +6,6 @@ void Fun1(int[] n)
         Console.WriteLine(i);
 }
 
-//Fun1(niz);
-
 int Fun2(int[] n)
 {
     int max = 0;
@@ -19,19 +17,6 @@ int Fun2(int[] n)
     return max;
 }
 
-int Fun21(int[] n)
-{
-    int min = n[0];
-    foreach (int i in n)
-    {
-        if (i < min)
-            min = i;
-    }
-    return min;
-}
-
-//int res = Fun2(niz);
-
 int[] Fun3(int[] n, int x)
 {
     for (int i = 0; i < n.Length; i++)
@@ -40,14 +25,6 @@ int[] Fun3(int[] n, int x)
     }
     return n;
 }
-
-//int[] res = Fun3(niz,2);
-
-//Fun1(res);
-
-// if(el % 2 == 0)
-// prani => el = el + a
-// neparni => el = el + b
 
 int[] Fun32(int[] n, int a, int b)
 {
